cc79f001b7d01d09b1b055120c5da9212481ed32
Rohan Singh <117467200+rohan4693@users.noreply.github.com>
2023-12-04 19:49:40 +0530

    Add files via upload

---
+++ JAVASCRIPT/Variables/script.js
@@ -0,0 +1,14 @@
+/*console.log("Hello World")
+var a=10;
+var b=30;
+var c="Hello World"
+console.log(a+b+c)
+console.log(typeof(a),typeof(b),typeof(c))
+*/
+
+alert("Hello World")
+
+// var x=102;
+// let x=10;
+
+// console.log(x)
